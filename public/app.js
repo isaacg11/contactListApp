fba2e06767c6cc3eb060db4608319967e0c91b5a
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-24 16:12:10 -0700

    register and login works for email/password

--- public/app.js
+++ public/app.js
@@ -1,2 +1,35 @@
 Stamplay.init('contactlistapp');
 
+//REGISTER W/EMAIL & PASSWORD
+function register(){
+	username = document.getElementById('usernameRegister').value;
+	email = document.getElementById('emailRegister').value;
+	password = document.getElementById('passwordRegister').value;
+
+	var registrationData = {
+  		displayname: username,
+  		email : email,
+  		password: password
+	};
+
+	var newUser = new Stamplay.User().Model;
+	newUser.signup(registrationData);
+
+	document.getElementById('usernameRegister').value = "";
+	document.getElementById('emailRegister').value = "";
+	document.getElementById('passwordRegister').value = "";
+}
+
+//LOGIN WITH EMAIL & PASSWORD
+function login(){
+	email = document.getElementById('emailLogin').value;
+	password = document.getElementById('passwordLogin').value;
+
+	var user = new Stamplay.User().Model;
+	user.login(email, password).then(function(){
+		email = document.getElementById('emailLogin').value = "";
+		password = document.getElementById('passwordLogin').value = "";
+	});
+
+}
+
